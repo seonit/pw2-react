829bb00528aafe30f7108bc78411a457fb80489c
seonit <seonit08@gmail.com>
2025-09-08 17:37:06 +0900

    250908 Redux

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,7 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
+import './Detail.css';
+import { addCnt, changeCnt, minusCnt } from '../store/slices/detail';
+import { useState } from 'react';
+
 function Detail() {
+  const dispatch = useDispatch();
+
+  const cnt = useSelector(state => state.detail.cnt);
+
+  const [inputNum, setInputNum] = useState(0);
+
+  const convertAndSetNumber = (e) => {
+    if(!isNaN(e.target.value)) {
+      setInputNum(parseInt(e.target.value));
+    } else {
+      console.error('숫자 아니다.');
+    }
+  }
+
   return (
     <>
-      <h2>상세 페이지</h2>
+      <h1>상세 페이지</h1>
+      <p>{cnt}</p>
+      <input type="number" onChange={convertAndSetNumber}/>
+      <button type="button" onClick={() => {dispatch(changeCnt(inputNum))}}>입력</button>
+      <button type="button" onClick={() => { dispatch(addCnt()) }}>+</button>
+      <button type="button" onClick={() => { dispatch(minusCnt()) }}>-</button>
     </>
   )
 }
